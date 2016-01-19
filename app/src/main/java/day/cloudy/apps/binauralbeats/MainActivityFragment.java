2ac8e09ab0637bd5b948878ada105ca35ce3480d
Gaelan Bolger <bolger.g@gmail.com>
2016-01-18 19:00:01 -0500

    Comment and clean

--- app/src/main/java/day/cloudy/apps/binauralbeats/MainActivityFragment.java
+++ app/src/main/java/day/cloudy/apps/binauralbeats/MainActivityFragment.java
@@ -27,15 +27,15 @@ import butterknife.ButterKnife;
 
 public class MainActivityFragment extends Fragment {
 
-    private static final ImmutableMap<Double, String> tones = ImmutableMap
-            .<Double, String>builder()
+    private static final ImmutableMap<Double, String> tones = ImmutableMap.<Double, String>builder()
             .put(55.00, "A1").put(61.74, "B1").put(65.41, "C2").put(73.41, "D2").put(82.41, "E2").put(87.31, "F2").put(98.00, "G2")
             .put(110.00, "A2").put(123.47, "B2").put(130.81, "C3").put(146.83, "D3").put(164.81, "E3").put(174.61, "F3").put(196.00, "G3")
             .put(220.00, "A3").put(246.94, "B3").put(261.63, "C4").put(293.66, "D4").put(329.63, "E4").put(349.23, "F4").put(392.00, "G4")
             .put(440.00, "A4").put(493.88, "B4").put(523.25, "C5").put(587.33, "D5").put(659.25, "E5").put(698.46, "F5").put(783.99, "G5")
             .put(880.00, "A5")
             .build();
-    private static final int SAMPLE_RATE = 16000;
+    private static final int SAMPLE_RATE = 8000;
+    private static final int AMP = 32767;
 
     @Bind(R.id.left_text)
     TextView mLeftText;
@@ -50,11 +50,10 @@ public class MainActivityFragment extends Fragment {
 
     private int minProgress = 5500;
     private int maxProgress = 88000 - minProgress;
-    private boolean stopped = false;
+    private boolean playing = false;
 
     private short sampleL[] = new short[SAMPLE_RATE];
     private short sampleR[] = new short[SAMPLE_RATE];
-    private int amp = 32767;
     private double twoPi = 8.0 * Math.atan(1.0);
     private double phL = 0.0;
     private double phR = 0.0;
@@ -128,33 +127,38 @@ public class MainActivityFragment extends Fragment {
                     AudioTrack.MODE_STREAM);
 
         audioTrack.play();
+        playing = true;
+
         new Thread(new Runnable() {
             @Override
             public void run() {
-                while (!stopped) {
+                while (playing) {
                     short[] tone = generateTone(freqL, freqR);
                     audioTrack.write(tone, 0, tone.length);
                 }
             }
         }).start();
-
-        stopped = false;
     }
 
     void stopPlaying() {
         if (null != audioTrack) {
             audioTrack.stop();
             audioTrack.flush();
+            audioTrack = null;
         }
 
-        stopped = true;
+        playing = false;
     }
 
+    /*
+    * https://audioprograming.wordpress.com/2012/10/18/a-simple-synth-in-android-step-by-step-guide-using-the-java-sdk/
+    * http://dsp.stackexchange.com/a/973
+    */
     short[] generateTone(double freqL, double freqR) {
         for (int i = 0; i < SAMPLE_RATE; ++i) {
-            sampleL[i] = (short) (amp * Math.sin(phL));
+            sampleL[i] = (short) (AMP * Math.sin(phL));
             phL += twoPi * freqL / SAMPLE_RATE;
-            sampleR[i] = (short) (amp * Math.sin(phR));
+            sampleR[i] = (short) (AMP * Math.sin(phR));
             phR += twoPi * freqR / SAMPLE_RATE;
         }
 
